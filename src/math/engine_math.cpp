04b5da230c1a387f7507385ec5eb9e172dcb0c29
werl <peter@werl.me>
2022-11-01 13:43:17 -0600

    Add math classes for future use

---
+++ src/math/engine_math.cpp
@@ -0,0 +1,8 @@
+//
+// Created by peter on 2022-11-01.
+//
+
+#include "engine_math.hpp"
+
+namespace engine::math {
+} // engine::math
---
+++ src/math/math_scaler.hpp
@@ -0,0 +1,18 @@
+//
+// Created by peter on 2022-11-01.
+//
+
+#ifndef VULKANENGINE_MATH_SCALER_HPP
+#define VULKANENGINE_MATH_SCALER_HPP
+
+#include "engine_math.hpp"
+
+namespace engine::math {
+
+    class MathScaler : EngineMath {
+
+    };
+
+} // engine::math
+
+#endif //VULKANENGINE_MATH_SCALER_HPP
